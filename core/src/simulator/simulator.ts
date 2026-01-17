85bce9120b798a73f2fc070a938644db00500ba6
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2026-01-17 20:35:55 +0300

    update

--- core/src/simulator/simulator.ts
+++ core/src/simulator/simulator.ts
@@ -1,26 +1,103 @@
+import { TapeSize } from "../enum/tapeSize";
 import { Graph } from "../parser/graph";
+
+export interface StepResult {
+  accepted: boolean;
+  rejected: boolean;
+  continue: boolean;
+}
 
 export class Simulator {
   public graph: Graph;
-  public tape1 = new Map<number, string>();
-  public tape2 = new Map<number, string>();
-  public tape3 = new Map<number, string>();
+  public tapes: Map<number, string>[];
+  public heads: number[];
   public currentState: string = undefined;
 
-  public init(graph: Graph) {
+  public init(graph: Graph, tapeSize: TapeSize) {
     this.graph = graph;
-    this.tape1.clear();
-    this.tape2.clear();
-    this.tape3.clear();
+    if (tapeSize === TapeSize.SINGLE) {
+      this.tapes = [new Map<number, string>()];
+      this.heads = [0];
+    } else if (tapeSize === TapeSize.DOUBLE) {
+      this.tapes = [new Map<number, string>(), new Map<number, string>()];
+      this.heads = [0, 0];
+    } else if (tapeSize === TapeSize.TRIPLE) {
+      this.tapes = [
+        new Map<number, string>(),
+        new Map<number, string>(),
+        new Map<number, string>(),
+      ];
+      this.heads = [0, 0, 0];
+    }
     this.currentState = this.graph.startState;
   }
 
-  public step(): boolean {
+  public step(): StepResult {
+    const result: StepResult = {
+      accepted: false,
+      rejected: false,
+      continue: false,
+    };
     const curNode = this.graph.nodes.get(this.currentState);
     if (!curNode) {
-      return false; // rejected
+      result.rejected = true;
+      return result;
     }
 
-    return true;
+    for (const transition of curNode.transitions) {
+      const tapeSymbols = this.getTapeSymbols();
+
+      const match = transition.read.every(
+        (symbol, index) => symbol === tapeSymbols[index] || symbol === "_", // _ matches everything
+      );
+
+      if (match) {
+        // write symbols
+        for (let i = 0; i < this.heads.length; i++) {
+          const writeSymbol = transition.write[i];
+          this.tapes[i].set(this.heads[i], writeSymbol);
+        }
+
+        // move heads
+        for (let i = 0; i < this.heads.length; i++) {
+          const moveDirection = transition.move[i];
+          if (moveDirection == ">") {
+            this.heads[i]++;
+          } else if (moveDirection == "<") {
+            this.heads[i]--;
+          } else if (moveDirection == "-") {
+            // do nothing
+          }
+        }
+
+        this.currentState = transition.toState;
+
+        // check if moved to accept state
+        if (this.graph.acceptStates.has(this.currentState)) {
+          result.accepted = true;
+          return result;
+        }
+
+        result.continue = true;
+        return result;
+      }
+    }
+
+    if (this.graph.acceptStates.has(this.currentState)) {
+      result.accepted = true;
+      return result;
+    }
+
+    result.rejected = true;
+    return result;
+  }
+
+  private getTapeSymbols(): string[] {
+    const symbols: string[] = [];
+
+    for (let i = 0; i < this.heads.length; i++) {
+      symbols.push(this.tapes[i].get(this.heads[i]) || "_"); // _ represents blank symbol (EPS)
+    }
+    return symbols;
   }
 }
---
+++ core/test/simulator.test.ts
@@ -0,0 +1,251 @@
+import { describe, expect, test } from "@jest/globals";
+import { lineSplitter } from "../src/parser/lineSplitter";
+import { graphBuilder } from "../src/parser/graphBuilder";
+import { LineType } from "../src/parser/line";
+import { syntaxChecker } from "../src/parser/syntaxChecker";
+import { TapeSize } from "../src/enum/tapeSize";
+import { Simulator, StepResult } from "../src/simulator/simulator";
+import { assert } from "console";
+
+describe("Binary numbers divisible by 3", () => {
+  const input = `
+name: Binary numbers divisible by 3
+init: q0
+accept: qAccept
+
+q0,0
+q0,0,>
+
+q0,1
+q1,1,>
+
+q1,0
+q2,0,>
+
+q1,1
+q0,1,>
+
+q2,0
+q1,0,>
+
+q2,1
+q2,1,>
+
+q0,_
+qAccept,_,-
+    `.trim();
+
+  test("ACCEPT", () => {
+    const lines = lineSplitter(input);
+    syntaxChecker(lines, () => {}, TapeSize.SINGLE);
+    const graph = graphBuilder(lines);
+    const simulator = new Simulator();
+    simulator.init(graph, TapeSize.SINGLE);
+
+    simulator.tapes[0].set(0, "1");
+    simulator.tapes[0].set(1, "1");
+    simulator.tapes[0].set(2, "0");
+
+    let result: StepResult | null = null;
+
+    do {
+      result = simulator.step();
+
+      if (result.rejected) {
+        expect(false).toBe(true);
+        break;
+      }
+
+      // should accept
+      if (result.accepted) {
+        break;
+      }
+    } while (result.continue);
+  });
+
+  test("REJECT", () => {
+    const lines = lineSplitter(input);
+    syntaxChecker(lines, () => {}, TapeSize.SINGLE);
+    const graph = graphBuilder(lines);
+    const simulator = new Simulator();
+    simulator.init(graph, TapeSize.SINGLE);
+
+    simulator.tapes[0].set(0, "1");
+    simulator.tapes[0].set(1, "1");
+    simulator.tapes[0].set(2, "0");
+    simulator.tapes[0].set(3, "1");
+
+    let result: StepResult | null = null;
+
+    do {
+      result = simulator.step();
+
+      // should reject
+      if (result.rejected) {
+        break;
+      }
+
+      if (result.accepted) {
+        expect(true).toBe(false);
+        break;
+      }
+    } while (result.continue);
+  });
+
+  test("REJECT", () => {
+    const lines = lineSplitter(input);
+    syntaxChecker(lines, () => {}, TapeSize.SINGLE);
+    const graph = graphBuilder(lines);
+    const simulator = new Simulator();
+    simulator.init(graph, TapeSize.SINGLE);
+
+    simulator.tapes[0].set(0, "1");
+    simulator.tapes[0].set(1, "a");
+    simulator.tapes[0].set(2, "0");
+
+    let result: StepResult | null = null;
+
+    do {
+      result = simulator.step();
+
+      // should reject
+
+      if (result.rejected) {
+        break;
+      }
+
+      if (result.accepted) {
+        expect(true).toBe(false);
+        break;
+      }
+    } while (result.continue);
+  });
+});
+
+describe("Binary addition", () => {
+  const input = `
+name: Binary addition
+init: q0
+accept: q5
+
+q0,0,_,_
+q0,0,_,_,>,-,-
+
+q0,1,_,_
+q0,1,_,_,>,-,-
+
+q0,#,_,_
+q1,_,_,_,>,-,-
+
+q1,0,_,_
+q1,_,0,_,>,>,-
+
+q1,1,_,_
+q1,_,1,_,>,>,-
+
+q1,_,_,_
+q2,_,_,_,<,<,-
+
+q2,_,0,_
+q2,_,0,_,<,-,-
+
+q2,_,1,_
+q2,_,1,_,<,-,-
+
+q2,1,0,_
+q3,1,0,_,-,-,-
+
+q2,1,1,_
+q3,1,1,_,-,-,-
+
+q2,0,1,_
+q3,0,1,_,-,-,-
+
+q2,0,0,_
+q3,0,0,_,-,-,-
+
+q3,1,0,_
+q3,1,0,1,<,<,<
+
+q3,0,1,_
+q3,0,1,1,<,<,<
+
+q3,0,0,_
+q3,0,0,0,<,<,<
+
+q3,1,1,_
+q4,1,1,0,<,<,<
+
+q3,_,_,_
+q5,_,_,_,-,-,-
+
+q3,1,_,_
+q3,1,_,1,<,<,<
+
+q3,0,_,_
+q3,0,_,0,<,<,<
+
+q3,_,1,_
+q3,_,1,1,<,<,<
+
+q3,_,0,_
+q3,_,0,0,<,<,<
+
+q4,0,0,_
+q3,0,0,1,<,<,<
+
+q4,0,1,_
+q4,0,1,0,<,<,<
+
+q4,1,0,_
+q4,1,0,0,<,<,<
+
+q4,1,1,_
+q4,1,1,1,<,<,<
+
+q4,_,0,_
+q3,_,0,1,<,<,<
+
+q4,_,1,_
+q4,_,1,0,<,<,<
+
+q4,1,_,_
+q4,1,_,0,<,<,<
+
+q4,0,_,_
+q3,0,_,1,<,<,<
+
+q4,_,_,_
+q5,_,_,1,-,-,-
+    `.trim();
+
+  test("ACCEPT", () => {
+    const lines = lineSplitter(input);
+    syntaxChecker(lines, () => {}, TapeSize.TRIPLE);
+    const graph = graphBuilder(lines);
+    const simulator = new Simulator();
+    simulator.init(graph, TapeSize.TRIPLE);
+
+    // Input: 101#110 (5 + 6)
+    simulator.tapes[0].set(0, "1");
+    simulator.tapes[0].set(1, "0");
+    simulator.tapes[0].set(2, "1");
+    simulator.tapes[0].set(3, "#");
+    simulator.tapes[0].set(4, "1");
+    simulator.tapes[0].set(5, "1");
+    simulator.tapes[0].set(6, "0");
+
+    let result: StepResult | null = null;
+    do {
+      result = simulator.step();
+      if (result.rejected) {
+        expect(false).toBe(true);
+        break;
+      }
+      // should accept
+      if (result.accepted) {
+        break;
+      }
+    } while (result.continue);
+  });
+});
